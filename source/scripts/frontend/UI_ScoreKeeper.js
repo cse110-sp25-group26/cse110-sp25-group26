f560c4a12e235364c357c352111a913fa44d492a
Tony <anthonyvelikov@gmail.com>
2025-05-18 14:51:02 -0700

    small tweak

--- source/scripts/frontend/UI_ScoreKeeper.js
+++ source/scripts/frontend/UI_ScoreKeeper.js
@@ -1,29 +1,19 @@
 export class ScoreKeeeper {
   constructor() {
-     this.round   = document.getElementById('round-score');
-    this.goal    = document.getElementById('goal-score');
-    this.chip    = document.getElementById('chip-val');
-    this.mult    = document.getElementById('mult-val');
-    this.bank    = document.getElementById('bank-amt');
-    this.hands   = document.getElementById('hands-val');
-    this.dis     = document.getElementById('discards-val');
-    this.ante    = document.getElementById('ante-val');
-    this.roundNo = document.getElementById('round-val');
-    this.cardCt  = document.getElementById('card-count');
-    this.deckCt  = document.getElementById('deck-count');
+     
     
 
     // initialize some variables like this.scoreElement, this.chipsElement, etc.
 
   }
 
-  function updateChips(chips) {
-  this.chipsElement.innerHTML = String(chips);
-}
+//   function updateChips(chips) {
+//   this.chipsElement.innerHTML = String(chips);
+// }
 
-function updateHands(used, total) {
-  this.handsElement.innerHTML = String(used) + "/" + String(total);
-}
+// function updateHands(used, total) {
+//   this.handsElement.innerHTML = String(used) + "/" + String(total);
+// } example
 
   // etc for all the scoreboard items
 }
